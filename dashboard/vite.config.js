674a940c4478b4f5153c0acd57790046c976cc7b
Nino <ninock1981@gmail.com>
2025-04-20 09:43:46 +0200

    Fix: build script added and working frontend

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,9 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // <-- Vercel očekuje dist folder
+  },
   server: {
-    port: 3000
+    port: 5173,
+    open: true
   }
 });
